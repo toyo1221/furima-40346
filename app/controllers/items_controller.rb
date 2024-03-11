c0f5cc22e38d10fbfa235debf6de50a2e5e3090d
toyo1221 <iyankukku1221@icloud.com>
2024-03-11 19:23:00 +0900

    コントローラの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,25 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  
+  def create
+    @item = current_user.items.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  
+
+
+ private
+  def item_params
+    params.require(:item).permit(:item_name, :description, :category_id, :situation_id, :delivery_charge_id, :prefecture_id, :delivery_day_id, :price)
+  end
 end
