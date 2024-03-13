ad1dfd8e4b69881eb79165250fe86674034ec5ea
toyo1221 <iyankukku1221@icloud.com>
2024-03-13 16:19:15 +0900

    再修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -9,7 +9,7 @@ FactoryBot.define do
     delivery_day_id { 1 } 
     price { 1000 }
     association :user
-    after(:build) do |message|
+    after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
     end
   end
