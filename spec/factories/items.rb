e125db74622fe7792e8640db5be6e5fb6028e4f4
toyo1221 <iyankukku1221@icloud.com>
2024-03-13 16:18:25 +0900

    factorybotの修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -10,7 +10,7 @@ FactoryBot.define do
     price { 1000 }
     association :user
     after(:build) do |message|
-      message.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
     end
   end
 end
